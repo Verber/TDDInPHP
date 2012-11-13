9ba5c97b84ca8f551f200e181b2b08eb8f3f89d1
Ivan Mosiev <i.k.mosev@gmail.com>
2012-11-13 13:26:30 +0200

    Fixed wrong logic (braces)

--- web/app_dev.php
+++ web/app_dev.php
@@ -14,11 +14,10 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
             in_array(@$_SERVER['REMOTE_ADDR'], array(
                 '127.0.0.1',
                 '::1',
-            )
+            ))
             || preg_match('/^192\.168\.[0-9.]+$/',
                     @$_SERVER['REMOTE_ADDR'])
         )
-    )
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
